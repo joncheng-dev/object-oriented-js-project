5b4918e0acc7664a33bdea789b31f8cd7337bb92
Jonathan Cheng <jcheng461@gmail.com>
2022-04-20 23:34:14 +0900

    add buttons, including one which adds current order to cart. Order is displayed.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@
 // User Interface Logic
 $(document).ready(function () {
   // After document is loaded.. do this.
+  $("#addToCart").click(function () {
+    $(".cartItems").append("<li>Pizza Order</li>");
+  });
 });
